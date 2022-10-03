27d20481cf02d54f62056520b0ea91d543f4e71a
N-Martinez0916 <nicomartin0916@gmail.com>
2022-10-03 11:37:30 -0400

    second commit

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // Your code here
+// const ul = document.getElementById('the-ul')
+// const newLi = document.createElement('li')
+// ul.appendChild(newLi)
+
+const addRow = document.getElementById('add-row')
+addRow.addEventListener('click', makeRow)
+
+const table = document.getElementById('table')
+function makeRow() {
+const row = document.createElement('tr')
+for (let i = 0; i < 20; i++) {
+const td = document.createElement('td')
+row.appendChild(td)
+}
+table.appendChild(row)
+}
